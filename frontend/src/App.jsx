80dde0b6b9089f6739767469e6e201ba196ecaa7
PseudoMotivated <122965701+PseudoMotivated@users.noreply.github.com>
2024-08-29 18:59:52 +0200

    fcg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 const challenge = {
-  name: 'defeat the gay',
+  name: 'defeat the goblin',
   category: 'web',
   description: 'seek and destory',
   points: 69,
