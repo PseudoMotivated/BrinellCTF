19fce1f15f476b25e80ce285dffd99e63310da24
TheoLundberg07 <theo.lundberg@hotmail.com>
2024-09-01 21:38:10 +0200

    Css opt
    Started optimizing css, units + class

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,16 +3,24 @@ import Login from "./loginButton";
 function Header() {
   return (
     <div>
-    <header>
-      <h1>BrinellCTF</h1>
-      <nav>
-        <a href="">Hem</a>
-        <a href="">Om</a>
-        <a href="">Utmaningar</a>
-        <a href="">Resurser</a>
-      </nav>
-      <Login></Login>
-    </header>
+      <header>
+        <h1>BrinellCTF</h1>
+        <nav>
+          <a href="" className="navLink">
+            Hem
+          </a>
+          <a href="" className="navLink">
+            Om
+          </a>
+          <a href="" className="navLink">
+            Utmaningar
+          </a>
+          <a href="" className="navLink">
+            Resurser
+          </a>
+        </nav>
+        <Login></Login>
+      </header>
     </div>
   );
 }
