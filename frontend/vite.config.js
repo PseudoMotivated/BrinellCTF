29c302d508ef99500a288f8d9d3c97abc0dd743f
PseudoMotivated <122965701+PseudoMotivated@users.noreply.github.com>
2024-09-14 22:06:27 +0200

    Vite build outs to pocketbase public dir

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../Boonis',
+    outDir: '../../pb/pb_public/',
     emptyOutDir: true
   }
 })
