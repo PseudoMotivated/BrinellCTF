ce7fc98c3a25fea86a54cced7b04041573c9d052
TheoLundberg07 <theo.lundberg@hotmail.com>
2024-09-16 17:16:39 +0200

    Started implementing react router

---
+++ frontend/src/routes/root.jsx
@@ -0,0 +1,23 @@
+import ChallengeEntry from "./components/ChallengeEntry";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+const challenge = {
+  name: "bobobobg",
+  category: "web",
+  description: "seek and destory",
+  points: 69,
+  hints: ["bbb"],
+};
+
+function Root() {
+  return (
+    <div>
+      <Header className="hooder" />
+      {<ChallengeEntry challenge={challenge} className="challenge" />}
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default Root;
