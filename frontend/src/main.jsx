827b388963f8a5bd2582487dcc5ac1b2760c16c0
TheoLundberg07 <theo.lundberg@hotmail.com>
2024-09-17 10:06:07 +0200

    remove accidentally installed node modules foldr in root

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import Root from "./routes/root";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
